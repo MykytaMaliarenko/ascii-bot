fcc9f16e93e424f8b183a461648b906834f88e46
Nikita <mykytamaliarenko@gmail.com>
2019-11-29 13:08:16 +0200

    feat(greeting): added greeting on /start

--- handlers.py
+++ handlers.py
@@ -1,17 +1,31 @@
+import re
 from telegram.ext import Dispatcher, MessageHandler, Filters
 from telegram.update import Update, Message
 from telegram.ext.callbackcontext import CallbackContext
 
+GREETING = "Hi!\nI can encode text in ascii by pairing or unpairing " \
+           "and convert it to hex system.\n Just write text and you " \
+           "will see all available options.\n GL =)"
+
+ASCII_REGEX = r"[?!\().\dA-z]+"
+
 
 def start(update: Update, context: CallbackContext):
-    pass
+    message: Message = update.message
+    message.reply_text(GREETING)
+
+
+def text_input(update: Update, context: CallbackContext):
+    message: Message = update.message
+
+
 
 
 handlers = {
     "/start": start,
 }
 
-default_handler = start
+default_handler = text_input
 
 
 def register_handlers(dp: Dispatcher):
