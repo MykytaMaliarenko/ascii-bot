4d9f53781a3595d4e6b62330ccdba03d47829939
Nikita <mykytamaliarenko@gmail.com>
2019-11-29 14:42:06 +0200

    feat(on_text_input): added handling of user input

--- handlers.py
+++ handlers.py
@@ -2,12 +2,11 @@ import re
 from telegram.ext import Dispatcher, MessageHandler, Filters
 from telegram.update import Update, Message
 from telegram.ext.callbackcontext import CallbackContext
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 GREETING = "Hi!\nI can encode text in ascii by pairing or unpairing " \
-           "and convert it to hex system.\n Just write text and you " \
-           "will see all available options.\n GL =)"
-
-ASCII_REGEX = r"[?!\().\dA-z]+"
+           "and convert it to hex system.\nJust write text and you " \
+           "will see all available actions.\nGL =)"
 
 
 def start(update: Update, context: CallbackContext):
@@ -15,17 +14,40 @@ def start(update: Update, context: CallbackContext):
     message.reply_text(GREETING)
 
 
-def text_input(update: Update, context: CallbackContext):
+encode_ascii_action, decode_binary, decode_hex = range(3)
+
+ASCII_REGEX = r"^[?!\().\dA-z]+$"
+BINARY_REGEX = r"^[0-1]+$"
+HEX_REGEX = r"^[0-9A-e]+$"
+
+
+def on_text_input(update: Update, context: CallbackContext):
     message: Message = update.message
 
+    keyboard = []
+    if bool(re.fullmatch(ASCII_REGEX, message.text)):
+        keyboard.append([InlineKeyboardButton("encode in ascii", callback_data=encode_ascii_action)])
 
+    if bool(re.fullmatch(BINARY_REGEX, message.text)):
+        keyboard.append(([InlineKeyboardButton("decode from binary", callback_data=decode_binary)]))
+
+    if bool(re.fullmatch(HEX_REGEX, message.text)):
+        keyboard.append([InlineKeyboardButton("decode from hex", callback_data=decode_hex)])
+
+    message.reply_text("Available Actions:", reply_markup=InlineKeyboardMarkup(keyboard), quote=True)
 
 
 handlers = {
     "/start": start,
 }
 
-default_handler = text_input
+default_handler = on_text_input
+
+callback_handlers = {
+    encode_ascii_action: None,
+    decode_binary: None,
+    decode_hex: None,
+}
 
 
 def register_handlers(dp: Dispatcher):
