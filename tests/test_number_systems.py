e81c22e065a339926243f2d8274c93c9501c5083
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2019-12-03 18:26:19 +0200

    feat(tests): add test for number_systems

---
+++ tests/test_number_systems.py
@@ -0,0 +1,31 @@
+import number_systems as np
+
+
+class TestNumberSystems:
+    def test_text_to_ascii_paired(self):
+        converter = np.get_converter(np.TEXT, np.ASCII)
+        res = "01001011 11100001 01110100 01100101"
+        assert converter("Kate", mode=np.BY_PAIRED).replace(" ", "") == res.replace(" ", "")
+
+    def test_text_to_ascii_unpaired(self):
+        converter = np.get_converter(np.TEXT, np.ASCII)
+        res = "1100 1011 0110 0001 1111 0100 1110 0101"
+        assert converter("Kate", mode=np.BY_UNPAIRED).replace(" ", "") == res.replace(" ", "")
+
+    def test_ascii_to_hex(self):
+        converter = np.get_converter(np.ASCII, np.HEX)
+        input_data = "1100 1011 0110 0001 1111 0100 1110 0101"
+        res = "cb 61 f4 e5"
+        assert converter(input_data).replace(" ", "") == res.replace(" ", "")
+
+    def test_ascii_to_text(self):
+        assert True == False
+
+    def test_hex_to_binary(self):
+        assert True == False
+
+    def test_binary_to_text(self):
+        assert True == False
+
+    def test_binary_to_ascii(self):
+        assert True == False
