9c1c4af018ef9d0db9f660607e08ba2fd375bbc3
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2019-12-03 19:08:01 +0200

    feat(number_systems): add binary_to_text

--- number_systems/binary.py
+++ number_systems/binary.py
@@ -1,6 +1,36 @@
+from .helpers import prepare_text
+from .text import BY_UNPAIRED, BY_PAIRED
+
+
 def binary_to_text(binary: str) -> str:
-    return ""
+    binary = prepare_text(binary)
+    res = []
+
+    while len(binary) > 0:
+        t = binary[:7]
+        t = int(t, 2)
+        res.append(chr(t))
+        binary = binary[7:]
+
+    return "".join(res)
 
 
-def binary_to_ascii(binary: str) -> str:
-    return ""
+def binary_to_ascii(binary: str, mode: int = BY_PAIRED) -> str:
+    binary = prepare_text(binary)
+    res = []
+
+    while len(binary) > 0:
+        t = binary[:7]
+        if t.count("1") % 2 == 0:
+            if mode == BY_PAIRED:
+                res.append("0" + t)
+            else:
+                res.append("1" + t)
+        else:
+            if mode == BY_PAIRED:
+                res.append("1" + t)
+            else:
+                res.append("0" + t)
+        binary = binary[7:]
+
+    return " ".join(res)
--- tests/test_number_systems.py
+++ tests/test_number_systems.py
@@ -30,7 +30,19 @@ class TestNumberSystems:
         assert converter(input_data).replace(" ", "") == res.replace(" ", "")
 
     def test_binary_to_text(self):
-        assert True == False
+        converter = np.get_converter(np.BINARY, np.TEXT)
+        input_data = "1001011 1100001 1110100 1100101"
+        res = "Kate"
+        assert converter(input_data).replace(" ", "") == res.replace(" ", "")
 
-    def test_binary_to_ascii(self):
-        assert True == False
+    def test_binary_to_ascii_paired(self):
+        converter = np.get_converter(np.BINARY, np.ASCII)
+        input_data = "1001011 1100001 1110100 1100101"
+        res = "01001011 11100001 01110100 01100101"
+        assert converter(input_data, mode=np.BY_PAIRED).replace(" ", "") == res.replace(" ", "")
+
+    def test_binary_to_ascii_unpaired(self):
+        converter = np.get_converter(np.BINARY, np.ASCII)
+        input_data = "1001011 1100001 1110100 1100101"
+        res = "1100 1011 0110 0001 1111 0100 1110 0101"
+        assert converter(input_data, mode=np.BY_UNPAIRED).replace(" ", "") == res.replace(" ", "")
